5a2948088d1559ed8b3972b15ffa958804bfb645
dcchoi <cheek0805@naver.com>
2022-03-25 13:53:32 +0900

    통계부분 템플릿 완성

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const sequelize = require('sequelize');
+const { group, student, attendance } = require('../models/index').models;
+
 router.get('/', (req, res, next) => {
 	res.send({ title: 'express'});
 });
