3074646cc4ef3f8be23ca80e0a180dab60b35d2e
dcchoi <cheek0805@naver.com>
2022-06-05 22:00:00 +0900

    add statistics prototype

--- routes/statistics.js
+++ routes/statistics.js
@@ -4,8 +4,19 @@ const router = express.Router();
 const sequelize = require('sequelize');
 const { group, student, attendance } = require('../models/index').models;
 
-router.get('/', (req, res, next) => {
-	res.send({ title: 'express'});
+router.get('/studentCountForMonth', (req, res, next) => {
+});
+
+router.get('/studentCountForWeek', (req, res, next) => {
+});
+
+router.get('/excellentStudent', (req, res, next) => {
+});
+
+router.get('/groupCountForMonth', (req, res, next) => {
+});
+
+router.get('/groupCountForWeek', (req, res, next) => {
 });
 
 module.exports = router;
